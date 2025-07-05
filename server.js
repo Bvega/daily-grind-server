7667750f614f0e5744814344452a283b63fccaa6
Bvega <bolivar.vega@gmail.com>
2025-07-04 23:07:05 -0400

    final commit: complete express server project with pages, readme, and gitignore

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
